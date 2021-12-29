c2c71bb2773cf1f086780fbef21dd34e4d22f95a
王 玉才 <netowls-studio@outlook.com>
2021-12-29 14:50:04 +0800

    chore: <by wangyucai 2021-12-29 14:49>  Microsoft VisualStudio 编译后改了程序集版本号。

--- M2Mqtt/Properties/AssemblyInfo.cs
+++ M2Mqtt/Properties/AssemblyInfo.cs
@@ -3,11 +3,11 @@ Copyright (c) 2013, 2014 Paolo Patierno
 
 All rights reserved. This program and the accompanying materials
 are made available under the terms of the Eclipse Public License v1.0
-and Eclipse Distribution License v1.0 which accompany this distribution. 
+and Eclipse Distribution License v1.0 which accompany this distribution.
 
-The Eclipse Public License is available at 
+The Eclipse Public License is available at
    http://www.eclipse.org/legal/epl-v10.html
-and the Eclipse Distribution License is available at 
+and the Eclipse Distribution License is available at
    http://www.eclipse.org/org/documents/edl-v10.php.
 
 Contributors:
@@ -18,7 +18,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("M2Mqtt")]
@@ -33,12 +33,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2021.1.0.0")]
+[assembly: AssemblyVersion("2021.1.8033.12180")]
 // to avoid compilation error (AssemblyFileVersionAttribute doesn't exist) under .Net CF 3.5
 #if !WindowsCE
-[assembly: AssemblyFileVersion("2021.1.0.0")]
+[assembly: AssemblyFileVersion("2021.1.8033.0")]
 #endif
